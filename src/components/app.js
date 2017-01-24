5332f3cb2be12c69bb0ff22322e2ea648575488e
Corey Keller <CKeller@habaneroconsulting.com>
2017-01-24 16:52:51 -0700

    Initial Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import BookList from '../containers/book-list'
+// .. means go up a directory
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+        <BookList />
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Fifty Shades of Grey' },
+        { title: 'Last Exit to Brooklyn' },
+        { title: 'Kitchen Confidential' },
+        { title: 'A Quiet American' }
+    ]
+}
